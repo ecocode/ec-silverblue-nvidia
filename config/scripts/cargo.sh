945513426920de4deec6da5b1856069d13e8b917
erik colson <eco@ecocode.net>
2024-02-06 22:24:40 +0100

    First version after update

---
+++ config/scripts/cargo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+echo 'This script will install modules from cargo.io'
+
+ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
+
+cargo install --root /usr nu
+cargo install --root /usr swayr
